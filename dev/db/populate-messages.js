bb193b84d25cd5385b912754507ec222affe8ed6
JawadAhmed-Pro <ja358288@gmail.com>
2025-01-16 08:51:18 +0500

    Update populate-messages.js

--- dev/db/populate-messages.js
+++ dev/db/populate-messages.js
@@ -1,1 +1,9 @@
-
+db.messages.insertMany([
+  { message: "Welcome to the Docker World .. Jawad Ahmed apka suhagat krty hein !" },
+  { message: "We're whaley glad you're here!" },
+  { message: "Whale, whale, whale, what have we here?" },
+  { message: "hello there!" },
+  { message: "So nice to see you!" },
+  { message : "Let's play some games to have with you ."},
+  { message : "Paper m Cloud Fedration wala question nai parha tha mam ."},
+]);
